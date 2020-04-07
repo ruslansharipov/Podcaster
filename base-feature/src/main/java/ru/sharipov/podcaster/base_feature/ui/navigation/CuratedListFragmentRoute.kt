15801b1a9f1169d7f8c91d9cd7459b4d6e943137
Ruslan Sharipov <sharipov@surfstudio.co>
2020-04-07 18:54:41 +0300

    fix of curated list route

--- base-feature/src/main/java/ru/sharipov/podcaster/base_feature/ui/navigation/CuratedListFragmentRoute.kt
+++ base-feature/src/main/java/ru/sharipov/podcaster/base_feature/ui/navigation/CuratedListFragmentRoute.kt
@@ -1,8 +1,9 @@
 package ru.sharipov.podcaster.base_feature.ui.navigation
 
 import ru.surfstudio.android.core.ui.navigation.feature.route.feature.FragmentCrossFeatureRoute
+import ru.surfstudio.android.core.ui.navigation.fragment.route.RootFragmentRoute
 
-class CuratedListFragmentRoute: FragmentCrossFeatureRoute() {
+class CuratedListFragmentRoute: FragmentCrossFeatureRoute(), RootFragmentRoute {
 
     override fun targetClassPath(): String {
         return "ru.sharipov.podcast.f_curated_list.CuratedListFragmentView"
