5d73d9584f607ad4559d1a3e61e26483ad6e54e5
Ruslan Sharipov <sharipov@surfstudio.co>
2020-04-11 19:44:41 +0300

    improved explore screen

--- f-curated-list/src/main/java/ru/sharipov/podcast/f_curated_list/CuratedItemController.kt
+++ f-curated-list/src/main/java/ru/sharipov/podcast/f_curated_list/CuratedItemController.kt
@@ -16,8 +16,6 @@ class CuratedItemController(
     private val podcastClickListener: (PodcastShort) -> Unit
 ) : BindableItemController<CuratedItem, CuratedItemController.Holder>() {
 
-    private val viewPool = RecyclerView.RecycledViewPool()
-
     override fun getItemId(data: CuratedItem): String = data.id
 
     override fun createViewHolder(parent: ViewGroup): Holder = Holder(parent)
@@ -39,7 +37,6 @@ class CuratedItemController(
         init {
             allBtn.setOnClickListener { payload?.let(allClickListener) }
             podcastsRv.run {
-                setRecycledViewPool(viewPool)
                 layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
                 adapter = easyAdapter
             }
