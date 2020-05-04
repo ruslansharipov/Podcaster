835ea8c32b0959f950518d77f55faf529a5caf0b
Ruslan Sharipov <sharipov@surfstudio.ru>
2020-05-04 20:26:11 +0300

    moved EpisodeView to Widget

--- base-feature/src/main/java/ru/sharipov/podcaster/base_feature/ui/widget/episode/EpisodeView.kt
+++ base-feature/src/main/java/ru/sharipov/podcaster/base_feature/ui/widget/episode/EpisodeView.kt
@@ -3,25 +3,43 @@ package ru.sharipov.podcaster.base_feature.ui.widget.episode
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.view_episode.view.*
 import org.threeten.bp.LocalTime
 import ru.sharipov.podcaster.base_feature.ui.util.EpisodeDateFormatter
 import ru.sharipov.podcaster.base_feature.R
 import ru.sharipov.podcaster.base_feature.ui.extesions.string
 import ru.sharipov.podcaster.domain.Episode
+import ru.surfstudio.android.core.mvp.binding.rx.ui.CoreRxConstraintLayoutView
+import ru.surfstudio.android.core.mvp.presenter.CorePresenter
+import ru.surfstudio.android.core.mvp.view.CoreView
 
 class EpisodeView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : CoreRxConstraintLayoutView(context, attrs, defStyleAttr) {
 
     private val dateFormatter = EpisodeDateFormatter(context)
+
+    private var episode: Episode? = null
 
     init {
         View.inflate(context, R.layout.view_episode, this)
         setBackgroundResource(R.drawable.bg_selectable_item_divider)
+    }
+
+    override fun createConfigurator() = EpisodeWidgetConfigurator()
+
+    override fun getName(): String = "EpisodeView"
+
+    override fun getPresenters() = emptyArray<CorePresenter<CoreView>>()
+
+    override fun getWidgetId(): String {
+        return episode?.id ?: super.getWidgetId()
+    }
+
+    override fun onBind() {
+
     }
 
     fun setEpisode(episode: Episode) {
